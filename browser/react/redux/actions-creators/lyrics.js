1e59e499461ef30cb1cacad7a05c62ec1303546b
Guillermo Aszyn <guilleaszyn@gmail.com>
2017-05-18 18:18:20 -0400

    added reducer and actions

---
+++ browser/react/redux/actions-creators/lyrics.js
@@ -0,0 +1,5 @@
+import { SET_LYRICS } from '../constants';
+
+export function setLyrics(text) {
+  return { type: SET_LYRICS, text };
+}
---
+++ browser/react/redux/reducers/root-reducer.js
@@ -0,0 +1,15 @@
+import { SET_LYRICS } from '../constants';
+
+const initialState = {
+  text: '',
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case (SET_LYRICS):
+      return Object.assign({}, state, { text: action.text });
+    default:
+      return state;
+  }
+}
+
