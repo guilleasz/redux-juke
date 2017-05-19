ae6aff48b10e6b9cc7cce188ae629772a5783ddd
Mike van Genderen <mikevg466>
2017-05-19 11:10:31 -0400

    Creates SetState functions for Lyrics Component

--- browser/react/containers/LyricsContainer.js
+++ browser/react/containers/LyricsContainer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import store from '../redux/store';
 import {setLyrics} from '../redux/actions-creators/lyrics';
+import Lyrics from '../components/Lyrics';
 
 export default class LyricsContainer extends React.Component{
   constructor(){
     super();
-    this.state = store.getState();
+    this.state = Object.assign({}, store.getState(), {artistQuery: '', songQuery: ''});
+    this.setSong = this.setSong.bind(this);
+    this.setArtist = this.setArtist.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount(){
     this.unsubscribe = store.subscribe(() => this.setState(store.getState()));
@@ -13,9 +17,19 @@ export default class LyricsContainer extends React.Component{
   componentWillUnmount(){
     this.unsubscribe();
   }
+  setSong(event){
+    this.setState({songQuery: event.target.value});
+  }
+  setArtist(event){
+    this.setState({artistQuery: event.target.value});
+  }
+  handleSubmit(event){
+    event.preventDefault();
+    console.log(this.state);
+  }
   render(){
     return (
-      <h1>Just a container, more to come!</h1>
+      <Lyrics {...this.state} setSong={this.setSong} setArtist={this.setArtist} handleSubmit={this.handleSubmit} />
     );
   }
 }
