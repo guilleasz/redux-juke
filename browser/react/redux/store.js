5bc74672c75aaec8d3b66eb4bcdd9e7b5db27d35
Mike van Genderen <mikevg466>
2017-05-19 11:33:48 -0400

    Adds Middleware to redux

--- browser/react/redux/store.js
+++ browser/react/redux/store.js
@@ -1,10 +1,15 @@
 /* globals window */
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers/root-reducer';
+import loggerMiddleware from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const  middleware = applyMiddleware(loggerMiddleware, thunkMiddleware);
 
 /* eslint-disable no-underscore-dangle */
-export default createStore(reducer,
-                           window.__REDUX_DEVTOOLS_EXTENSION__ &&
-                           window.__REDUX_DEVTOOLS_EXTENSION__());
+export default createStore(reducer, composeEnhancers(middleware));
 /* eslint-enable */
+
+;
